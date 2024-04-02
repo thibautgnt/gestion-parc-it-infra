4e35d10df0833a3cd292454cf3f06f5408227904
thibautgnt <thibaut0genet@gmail.com>
2024-04-02 15:19:41 +0200

    First commit

---
+++ src/main/java/com/efrei/gererparcitapp/GererParcItAppApplication.java
@@ -0,0 +1,13 @@
+package com.efrei.gererparcitapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GererParcItAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GererParcItAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/efrei/gererparcitapp/GererParcItAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efrei.gererparcitapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GererParcItAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
